1fc0f280246958ffe8c583700f4782d6c3b484b5
5687GAMG <AL07153680@gmail.com>
2026-06-28 23:01:45 -0600

    Add files via upload

--- src/train_actividad6.py
+++ src/train_actividad6.py
@@ -1,1 +1,46 @@
 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+
+df = pd.read_csv("datos/default_credit_card_limpio.csv")
+
+X = df.drop(columns=["default"])
+y = df["default"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+models = {
+    "Baseline": DummyClassifier(strategy="most_frequent"),
+    "Regresion_Logistica": Pipeline([
+        ("scaler", StandardScaler()),
+        ("model", LogisticRegression(max_iter=1000, class_weight="balanced"))
+    ]),
+    "Random_Forest": RandomForestClassifier(
+        n_estimators=200,
+        max_depth=10,
+        random_state=42,
+        class_weight="balanced"
+    )
+}
+
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_proba = model.predict_proba(X_test)[:, 1]
+
+    print(name)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred, zero_division=0))
+    print("Recall:", recall_score(y_test, y_pred, zero_division=0))
+    print("F1-score:", f1_score(y_test, y_pred, zero_division=0))
+    print("AUC:", roc_auc_score(y_test, y_proba))
+    print("-" * 50)
